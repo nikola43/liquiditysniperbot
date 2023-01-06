8cb48fc0c7f7c815aee694c61dc81d39f7e54c86
Paulo <pauloxti@gmail.com>
2023-01-06 18:58:14 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	ierc20 "github.com/nikola43//liquiditysniperbot/contracts/IERC20"
+	pancakeFactory "github.com/nikola43//liquiditysniperbot/contracts/IPancakeFactory"
+	pancakePair "github.com/nikola43//liquiditysniperbot/IPancakePair"
+	pancakeRouter "github.com/nikola43//liquiditysniperbot/IPancakeRouter02"
+)
+
+func main() {
+	
+}
